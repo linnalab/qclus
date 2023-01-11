9cd82525a01d2026c4ff49b4b32892435386b40e
Johannes Ojanen <johannes.ojanen@gmail.com>
2023-01-11 17:08:07 +0200

    removed superfluous functions, updated samples directory

--- qclus/qclus.py
+++ qclus/qclus.py
@@ -4,11 +4,9 @@ import scrublet as scr
 import loompy
 import scanpy as sc
 import anndata
-import os
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import MinMaxScaler
-sc.settings.verbosity = 0         # verbosity: errors (0), warnings (1), info (2), hints (3)
-import subprocess
+sc.settings.verbosity = 0   # verbosity: errors (0), warnings (1), info (2), hints (3)
 
 nucl_genes_50 = ['MALAT1', 'NEAT1', 'FTX', 'FOXP1', 'RBMS3', 'ZBTB20', 'LRMDA', 'PBX1', 'ITPR2', 'AUTS2', 'TTC28', 'BNC2', 'EXOC4', 'RORA', 'PRKG1', 'ARID1B', 'PARD3B', 'GPHN', 'N4BP2L2', 'PKHD1L1', 'EXOC6B', 'FBXL7', 'MED13L', 'TBC1D5', 'IMMP2L', 'SYNE1', 'RERE', 'MBD5', 'EXT1', 'WWOX', 'EPB41L4A', 'PTK2', 'ST6GAL1', 'CHD9', 'PTPRG', 'JMJD1C', 'WSB1', 'SBF2', 'STAG1', 'GMDS', 'ADAMTS9-AS2', 'PDE7B', 'RALGAPA2', 'PRKN', 'ZFAND3', 'PLXDC2', 'NAALADL2', 'ASAP1', 'NFIA', 'MIR99AHG']
 
@@ -40,59 +38,6 @@ nucl_gene_set_dict = {
 "nucl_2" : nucl_genes_50[:2],
 "nucl_1" : nucl_genes_50[:1]
 }
-
-def create_new_index(index):
-    barcodes = [x[0:16] for x in index.tolist()]
-    return barcodes
-
-def create_fraction_unspliced_metric(loompy_path, index):
-    loompy_con = loompy.connect(loompy_path)
-    barcodes = [x.split(':')[1][0:16] for x in loompy_con.ca['CellID'].tolist()]
-    spliced_df = pd.Series(loompy_con['spliced'][:,:].sum(0).tolist(), index = barcodes)
-    unspliced_df = pd.Series(loompy_con['unspliced'][:,:].sum(0).tolist(), index = barcodes)
-    ambiguous_df = pd.Series(loompy_con['ambiguous'][:,:].sum(0).tolist(), index = barcodes)
-    fraction_unspliced_df = unspliced_df / (spliced_df + unspliced_df + ambiguous_df)
-    barcodes_data = [x for x in index] 
-    loompy_con.close()
-    return fraction_unspliced_df[barcodes_data].values
-
-def do_kmeans(X_full, k):
-    X_minmax = MinMaxScaler().fit_transform(X_full)
-    X_minmax = pd.DataFrame(X_minmax, index = X_full.index, columns = X_full.columns)
-    kmeans_custom = KMeans(n_clusters=k, random_state=0).fit(X_minmax) 
-    X_full['kmeans'] = kmeans_custom.labels_.astype(str)
-
-    #Sort kmeans clusters 0,1,2,...,k by decreasing fraction_unspliced mean.
-    dflist = []
-    clusters = []
-    for i in range(k):
-        df = X_full[X_full.kmeans==str(i)]
-        dflist.append(df)
-    sorteddflist = sorted(dflist,key=lambda x:x["fraction_unspliced"]. mean(axis=0))
-    sorteddflist.reverse()
-    for dframe in sorteddflist:
-        clusters.append(str(int(dframe.kmeans.median())))
-    return [str(clusters.index(x)) for x in X_full.kmeans]
-
-def annotate_outliers(df, unspliced_diff, mito_diff):
-    #find kmeans cluster with highest mean pct_counts_nonCM
-    max_non_CM = 0
-    max_value = 0
-    for clus in set(df.kmeans):
-        if (df.loc[df.kmeans == clus, "pct_counts_nonCM"].mean() > max_value):
-            max_non_CM = clus
-            max_value = df.loc[df.kmeans == clus, "pct_counts_nonCM"].mean()
-            
-    #tag cells that are in the max_non_cm cluster as 1, others 0
-    df['max_non_CM'] = ["1" if x in max_non_CM else "0" for x in df.kmeans]
-    
-    #annotate cells as outlier ("1") or not outlier ("0")
-    return ["0" if 
-            ((fraction_unspliced > (df[df.max_non_CM == "1"].fraction_unspliced.quantile(.25) - unspliced_diff)) 
-             & (pct_counts_MT < (df[df.max_non_CM == "1"].pct_counts_MT.quantile(.75) + mito_diff))) 
-            else "1" 
-            for fraction_unspliced,pct_counts_MT 
-            in zip(df.fraction_unspliced, df.pct_counts_MT)]
 
 def qclus(outs_path, loompy_path, 
                     gene_set_dict=celltype_gene_set_dict, 
