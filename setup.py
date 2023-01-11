d36287110b7fdd72e0417dbad6636ce5482d1cd2
Johannes Ojanen <johannes.ojanen@gmail.com>
2023-01-11 18:34:21 +0200

    Added comment to setup file

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 setup(
     name='qclus',
-    #version='0.3.0',
+    #version='0.1.0',
     description='Description',
     url='https://github.com/johannesojanen/qclus',
     author='Eloi Schauch and Johannes Ojanen',
     author_email='johannes.ojanen@gmail.com',
-    #license='BSD 2-clause',
+    #license='BSD 2-clause', add later
     packages=find_packages(),
     install_requires=[r.strip() for r in Path("requirements.txt").read_text("utf-8").splitlines()],
 
