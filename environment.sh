d7a86e88f5c76af66034578a81782b93bb72b706
Johannes Ojanen <johannes.ojanen@gmail.com>
2024-06-20 15:18:53 +0300

    built environment.sh file and removed former .yml file.

---
+++ environment.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Step 1: Create the Conda environment
+echo "Creating the Conda environment..."
+conda create --name qclus python=3.11 -y
+
+# Step 2: Activate the environment
+echo "Activating the environment..."
+source activate qclus
+
+# Step 3: Install packages using Conda
+echo "Installing Conda packages..."
+conda install -c conda-forge scanpy python-igraph leidenalg loompy -y
+conda install -c conda-forge python-annoy -y
+conda install -c conda-forge jupyterlab  -y
+
+
+# Step 4: Install additional Python packages using pip
+echo "Installing Python packages using pip..."
+pip install .
+
+echo "Environment setup is complete."
